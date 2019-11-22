447218e3abd841f2d0af8f605cc06e40152a5d1b
bloodycoder <picardxie@foxmail.com>
2019-11-22 13:45:39 +0800

    show avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,8 @@ class Profile < ApplicationRecord
 
   # for paperclip for file attachment functionality:
   has_attached_file :avatar, styles: { medium: "175x175#", thumb: "100x100#", tac: "20x20#" },
-                             default_url: 'https://visit.nemedic.com/storage/default.jpg'
-  validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png"]
+                             default_url: 'https://wx3.sinaimg.cn/mw690/515a7464ly1g8pj3k5sctj20sg0izqbz.jpg'
+  validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png","image/gif"]
 
   validate  :birth_date_cannot_be_in_the_future
   validates :gender, length: { maximum: 100 }
@@ -12,7 +12,7 @@ class Profile < ApplicationRecord
   validates :bio, length: { maximum: 2000 }
 
   has_attached_file :cover, styles: { large: "851x358#" }
-  validates_attachment_content_type :cover, content_type: ["image/jpg", "image/jpeg", "image/png"]
+  validates_attachment_content_type :cover, content_type: ["image/jpg", "image/jpeg", "image/png","image/gif"]
 
   def birth_date_cannot_be_in_the_future
     errors.add(:birthday, "can't be in the future") if
