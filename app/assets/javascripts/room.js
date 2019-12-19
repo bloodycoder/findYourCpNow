285474c9bcc5a5ec27450b46c53d3b4c097c746c
bloodycoder <picardxie@foxmail.com>
2019-12-19 16:05:50 +0800

    change logo

--- app/assets/javascripts/room.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('#new_room_message').on('ajax:success', function(a, b,c ) {
-      $(this).find('input[type="text"]').val('');
-      document.getElementById("room_message_message").value="";
-    });
-  });
