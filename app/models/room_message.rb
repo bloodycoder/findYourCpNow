2c6d9b38af9f447f273f55a8247043938993a2fa
bloodycoder <picardxie@foxmail.com>
2019-12-19 16:41:59 +0800

    change bug

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -2,6 +2,7 @@ class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
   belongs_to :user
   def as_json(options)
-    super(options).merge(user_avatar_url: user.gravatar_url)
+    #byebug
+    super(options).merge(user_avatar_url: user.profile.profile_photo('tac'))
   end
 end
